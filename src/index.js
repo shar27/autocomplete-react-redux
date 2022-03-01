60c2b8ac5113a36ca8d607777066716b1ffdd61e
shar27 <shariq.ahmed731@gmail.com>
2022-03-01 11:11:51 +0100

    react redux

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-
+import {Provider} from 'react-redux'
 import App from './App';
 
 
 ReactDOM.render(
   <React.StrictMode>
+  
     <App />
+  
   </React.StrictMode>,
   document.getElementById('root')
 );
