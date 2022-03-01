7c7e9518f00a7da236668e9f4204968a67073ff9
shar27 <shariq.ahmed731@gmail.com>
2022-03-01 11:02:31 +0100

    autocomplete

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Input from './components/Input'
+import React from 'react'
+import ReactDOM from 'react-dom';
+
+function App() {
+
+  const hoverStyle = {
+    pointer: 'cursor'
+  };
+
+  return (
+    <div className="">
+     <Input/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Input.js
@@ -0,0 +1,70 @@
+
+import React from 'react'
+import {useState, useEffect} from 'react'
+
+function Input() {
+  
+   
+       
+  const [name, setName] = useState('');
+  const [options, setOptions] = useState([])
+  const [data, setData] = useState(false)
+  
+  useEffect(() => {
+    const newUsers = async () => {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users')
+       .then((res) => res.json())
+       .then((data)=> {
+         setData(data)
+       });
+        
+    };
+
+    newUsers();
+  }, [])
+  console.log(data);
+  
+  const handleChange = (name) => {
+  let userMatch = [];
+    if (name.length > 0) {
+    userMatch = data.filter(usr => {
+      const regex = new RegExp(`${name}`, "gi")
+      return usr.name.match(regex)
+    })
+  }
+  setOptions(userMatch)
+  console.log(userMatch);
+    setName(name)
+    
+  }  
+
+  const handleOptions =  (name) => {
+    setName(name)
+    setOptions([])
+  }
+ 
+  
+
+    return (
+    <div>
+    <div className='flex justify-center mt-10'>
+        <input id="input" className='w-96  h-16 border-1 shadow-lg rounded-lg' placeholder="Search..."
+       
+        onChange={e=> handleChange(e.target.value)}
+        value={name}
+          />
+       
+            </div>
+           
+        { options?.map((op) => (
+          <div onClick={()=> handleOptions(op.name)}  className=' bg-white hover:bg-slate-400 shadow-2xl border-2 flex justify-center'>
+            Did you mean? {op.name}
+          </div>
+          
+        ))}
+          
+    </div>
+  );
+    }
+
+export default Input
